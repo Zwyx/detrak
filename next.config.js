4f293109a5bc9291833c7d741f782354eaa42b01
Zwyx <29386932+Zwyx@users.noreply.github.com>
2023-06-30 13:59:29 +0800

    Conversion to Vite app to be able to use i18next – Create Vite app
    we give up Next's SSG, but it isn't a big deal for such a small app

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-	reactStrictMode: true,
-	// was used to host the app on GitHub Pages; not necessary with Netlify
-	// output: "export",
-	// distDir: "docs",
-};
-
-module.exports = nextConfig;
