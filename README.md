e6d83264487d6df2a53e2a02ec09191056cbde35
Zwyx <29386932+Zwyx@users.noreply.github.com>
2023-07-09 20:46:15 +0800

    Generate PWA assets with `pwa-asset-generator`

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+# To be tested
+
+- Ask someone with an apple device if transparent icons and splash screen work.
+
 # Notes
 
 Pour les donnations, essayer https://ko-fi.com/ ou BuyMeACoffee. Also: https://liberapay.com/
--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,34 @@ export default defineConfig({
 			},
 			registerType: "prompt",
 			manifestFilename: "manifest.json",
+			manifest: {
+				icons: [
+					{
+						src: "manifest-icon-192.maskable.png",
+						sizes: "192x192",
+						type: "image/png",
+						purpose: "any",
+					},
+					{
+						src: "manifest-icon-192.maskable.png",
+						sizes: "192x192",
+						type: "image/png",
+						purpose: "maskable",
+					},
+					{
+						src: "manifest-icon-512.maskable.png",
+						sizes: "512x512",
+						type: "image/png",
+						purpose: "any",
+					},
+					{
+						src: "manifest-icon-512.maskable.png",
+						sizes: "512x512",
+						type: "image/png",
+						purpose: "maskable",
+					},
+				],
+			},
 			// We'll probably need that for the fonts
 			// workbox: {
 			// 	globPatterns: ["**/*.{js,css,html,ico,png,svg,woff,woff2}"],
